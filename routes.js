2e988e990efa4698305018781197b0f9fe2d7116
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-28 12:23:14 +0800

    Copy relevant modules to routes module
    Copy routes to routes module
    
    Comment out routes in server.js

--- routes.js
+++ routes.js
@@ -1,4 +1,81 @@
+const passport = require('passport')
+const bcrypt = require('bcrypt')
 
 module.exports = function (app, db) { 
+  // API routes
+  app.route('/')
+    .get((req, res) => {
+      res.render(process.cwd() + '/views/pug/index',
+        {
+          title: 'Home page',
+          message: 'Please login',
+          showLogin: true,
+          showRegistration: true,
+        }
+      )
+    })
 
+  app.route('/login')
+    .post(passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res) => {
+        console.log('The login req.body contents are: ', req.body)
+        res.redirect('/profile')
+      })
+
+  // middleware to ensure user is authenticated before displaying profile page
+  const ensureAuthenticated = (req, res, next) => {
+    // isAuthenticated() via passport
+    console.log('req contents are: ', req.isAuthenticated())
+    if (req.isAuthenticated()) {
+      console.log('Is authenticated')
+      return next();
+    }
+    console.log('Is not authenticated')
+    res.redirect('/');
+  }
+
+  app.route('/profile')
+    .get(ensureAuthenticated, (req, res) => {
+      console.log('username is: ', req.user)
+      res.render(process.cwd() + '/views/pug/profile', { username: req.user.username })
+    })
+
+  app.route('/logout')
+    .get((req, res) => {
+      req.logout()
+      res.redirect('/')
+    })
+
+  app.route('/register')
+    .post((req, res, next) => {
+      db.collection('users').findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          console.log('User already exists')
+          res.redirect('/');
+        } else {
+          var hash = bcrypt.hashSync(req.body.password, 12)
+          db.collection('users').insertOne(
+            {
+              username: req.body.username,
+              // password: req.body.password},
+              password: hash
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect('/');
+              } else {
+                next(null, user);
+              }
+            }
+          )
+          console.log(`${req.body.username} created`)
+        }
+      })
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    })
 }
--- server.js
+++ server.js
@@ -14,7 +14,10 @@ const mongo = require('mongodb').MongoClient
 
 // user authentication module
 const auth = require('./auth.js')
-// const routes = require('./routes.js')
+
+//routing module
+const routes = require('./routes.js')
+
 const dbURI = process.env.DBCONNECT
 
 const app = express()
@@ -47,81 +50,82 @@ mongo.connect(dbURI, { useNewUrlParser: true }, (err, conn) => {
   }
 
   auth(app, db)
+  routes(app, db)
 
-  // API routes
-  app.route('/')
-    .get((req, res) => {
-      res.render(process.cwd() + '/views/pug/index',
-        {
-          title: 'Home page',
-          message: 'Please login',
-          showLogin: true,
-          showRegistration: true,
-        }
-      )
-    })
+  // // API routes
+  // app.route('/')
+  //   .get((req, res) => {
+  //     res.render(process.cwd() + '/views/pug/index',
+  //       {
+  //         title: 'Home page',
+  //         message: 'Please login',
+  //         showLogin: true,
+  //         showRegistration: true,
+  //       }
+  //     )
+  //   })
 
-  app.route('/login')
-    .post(passport.authenticate('local', { failureRedirect: '/' }),
-      (req, res) => {
-        console.log('The login req.body contents are: ', req.body)
-        res.redirect('/profile')
-      })
+  // app.route('/login')
+  //   .post(passport.authenticate('local', { failureRedirect: '/' }),
+  //     (req, res) => {
+  //       console.log('The login req.body contents are: ', req.body)
+  //       res.redirect('/profile')
+  //     })
 
-  // middleware to ensure user is authenticated before displaying profile page
-  const ensureAuthenticated = (req, res, next) => {
-    // isAuthenticated() via passport
-    console.log('req contents are: ', req.isAuthenticated())
-    if (req.isAuthenticated()) {
-      console.log('Is authenticated')
-      return next();
-    }
-    console.log('Is not authenticated')
-    res.redirect('/');
-  }
+  // // middleware to ensure user is authenticated before displaying profile page
+  // const ensureAuthenticated = (req, res, next) => {
+  //   // isAuthenticated() via passport
+  //   console.log('req contents are: ', req.isAuthenticated())
+  //   if (req.isAuthenticated()) {
+  //     console.log('Is authenticated')
+  //     return next();
+  //   }
+  //   console.log('Is not authenticated')
+  //   res.redirect('/');
+  // }
   
-  app.route('/profile')
-    .get(ensureAuthenticated, (req, res) => {
-      console.log('username is: ', req.user)
-      res.render(process.cwd() + '/views/pug/profile', { username: req.user.username })
-    })
+  // app.route('/profile')
+  //   .get(ensureAuthenticated, (req, res) => {
+  //     console.log('username is: ', req.user)
+  //     res.render(process.cwd() + '/views/pug/profile', { username: req.user.username })
+  //   })
 
-  app.route('/logout')
-    .get((req, res) => {
-      req.logout()
-      res.redirect('/')
-    })
+  // app.route('/logout')
+  //   .get((req, res) => {
+  //     req.logout()
+  //     res.redirect('/')
+  //   })
 
-  app.route('/register')
-    .post((req, res, next) => {
-      db.collection('users').findOne({ username: req.body.username }, (err, user) => {
-        if (err) {
-          next(err);
-        } else if (user) {
-          console.log('User already exists')
-          res.redirect('/');
-        } else {
-          var hash = bcrypt.hashSync(req.body.password, 12)
-          db.collection('users').insertOne(
-            {username: req.body.username,
-              // password: req.body.password},
-              password: hash},
-            (err, doc) => {
-              if (err) {
-                res.redirect('/');
-              } else {
-                next(null, user);
-              }
-            }
-          )
-          console.log(`${req.body.username} created`)
-        }
-      })
-    },
-    passport.authenticate('local', { failureRedirect: '/' }),
-      (req, res, next) => {
-        res.redirect('/profile');
-    })
+  // app.route('/register')
+  //   .post((req, res, next) => {
+  //     db.collection('users').findOne({ username: req.body.username }, (err, user) => {
+  //       if (err) {
+  //         next(err);
+  //       } else if (user) {
+  //         console.log('User already exists')
+  //         res.redirect('/');
+  //       } else {
+  //         var hash = bcrypt.hashSync(req.body.password, 12)
+  //         db.collection('users').insertOne(
+  //           {username: req.body.username,
+  //             // password: req.body.password},
+  //             password: hash},
+  //           (err, doc) => {
+  //             if (err) {
+  //               res.redirect('/');
+  //             } else {
+  //               next(null, user);
+  //             }
+  //           }
+  //         )
+  //         console.log(`${req.body.username} created`)
+  //       }
+  //     })
+  //   },
+  //   passport.authenticate('local', { failureRedirect: '/' }),
+  //     (req, res, next) => {
+  //       res.redirect('/profile');
+  //   })
   
   app.use((req, res, next) => {
     res.status(404)
